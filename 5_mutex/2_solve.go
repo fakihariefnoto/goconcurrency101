72def61ce28f3617bcb2812aadd2b72eb376831a
fakihariefnoto <fakihariefnoto@gmail.com>
2022-01-28 10:05:49 +0700

    add mutex example

--- 5_mutex/2_solve.go
+++ 5_mutex/2_solve.go
@@ -7,18 +7,21 @@ import (
 
 var x = 0
 
-func increment(wg *sync.WaitGroup) {
+func incrementSolve(wg *sync.WaitGroup, mu *sync.Mutex) {
 	defer wg.Done()
+	mu.Lock()
 	x = x + 1
+	mu.Unlock()
 	return
 }
 func main() {
 	var w sync.WaitGroup
+	var mu sync.Mutex
 
 	fmt.Println("Pengulangan sebanyak 10k untuk menambahkan variable x")
 	for i := 0; i < 10000; i++ {
 		w.Add(1)
-		go increment(&w)
+		go incrementSolve(&w, &mu)
 	}
 	w.Wait()
 
--- 5_mutex/3_whatif.go
+++ 5_mutex/3_whatif.go
@@ -7,18 +7,21 @@ import (
 
 var x = 0
 
-func increment(wg *sync.WaitGroup) {
+func incrementSolve(wg *sync.WaitGroup, c chan bool) {
 	defer wg.Done()
+	c <- false
 	x = x + 1
+	<-c
 	return
 }
 func main() {
 	var w sync.WaitGroup
+	ch := make(chan bool, 1)
 
 	fmt.Println("Pengulangan sebanyak 10k untuk menambahkan variable x")
 	for i := 0; i < 10000; i++ {
 		w.Add(1)
-		go increment(&w)
+		go incrementSolve(&w, ch)
 	}
 	w.Wait()
 
