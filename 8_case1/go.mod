16b29b7e8defd40aabbce1cd563971833619136d
Muhammad Hasbi Muharyadi <hasbi.muharyadi@outlook.com>
2022-01-28 10:46:15 +0700

    case 8

---
+++ 8_case1/go.mod
@@ -0,0 +1,3 @@
+module github.com/fakiehariefnoto/goconcurrency101/8_case1
+
+go 1.17
---
+++ 8_case1/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	data, err := getSequential(123)
+	if err != nil {
+		log.Printf("err %v", err)
+	}
+	log.Printf("data %+v", data)
+	log.Printf("response time %s", time.Since(start).String())
+
+	start = time.Now()
+	data, err = getConcurrent(123)
+	if err != nil {
+		log.Printf("err %v", err)
+	}
+	log.Printf("data %+v", data)
+	log.Printf("response time %s", time.Since(start).String())
+
+}
+
+func getConcurrent(productID int64) (data ProductDisplayPage, err error) {
+	var product Product
+	var media Media
+	var flashsale FlashSale
+	var insight Insight
+
+	var wg sync.WaitGroup
+	var mtx sync.Mutex
+	wg.Add(4)
+
+	go func() {
+		defer wg.Done()
+		var errGo error
+		product, errGo = GetProduct(productID)
+		if errGo != nil {
+			mtx.Lock()
+			err = errGo
+			mtx.Unlock()
+			return
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		var errGo error
+		media, errGo = GetMedia(productID)
+		if err != nil {
+			mtx.Lock()
+			err = errGo
+			mtx.Unlock()
+			return
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		var errGo error
+		flashsale, errGo = GetFlashsale(productID)
+		if err != nil {
+			mtx.Lock()
+			err = errGo
+			mtx.Unlock()
+			return
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		var errGo error
+		insight, errGo = GetInsight(productID)
+		if err != nil {
+			mtx.Lock()
+			err = errGo
+			mtx.Unlock()
+			return
+		}
+	}()
+	wg.Wait()
+
+	return ProductDisplayPage{
+		ID:          product.ID,
+		Name:        product.Name,
+		Description: product.Description,
+		Condition: func(conditionNum int64) string {
+			if conditionNum == 1 {
+				return "Baru"
+			}
+			return "Bekas"
+		}(product.Condition),
+		Price:           product.Price,
+		DiscountedPrice: flashsale.DiscountedPrice,
+		Rating:          insight.Rating,
+		SoldStats:       insight.SoldStats,
+		CountReview:     insight.CountReview,
+		Discussion:      insight.Discussion,
+		MainImage:       media.MainImage,
+		AdditionalImage: media.AdditionalImage,
+	}, nil
+}
+
+func getSequential(productID int64) (data ProductDisplayPage, err error) {
+	product, err := GetProduct(productID)
+	if err != nil {
+		return data, err
+	}
+
+	media, err := GetMedia(productID)
+	if err != nil {
+		return data, err
+	}
+
+	flashsale, err := GetFlashsale(productID)
+	if err != nil {
+		return data, err
+	}
+
+	insight, err := GetInsight(productID)
+	if err != nil {
+		return data, err
+	}
+
+	return ProductDisplayPage{
+		ID:          product.ID,
+		Name:        product.Name,
+		Description: product.Description,
+		Condition: func(conditionNum int64) string {
+			if conditionNum == 1 {
+				return "Baru"
+			}
+			return "Bekas"
+		}(product.Condition),
+		Price:           product.Price,
+		DiscountedPrice: flashsale.DiscountedPrice,
+		Rating:          insight.Rating,
+		SoldStats:       insight.SoldStats,
+		CountReview:     insight.CountReview,
+		Discussion:      insight.Discussion,
+		MainImage:       media.MainImage,
+		AdditionalImage: media.AdditionalImage,
+	}, nil
+}
